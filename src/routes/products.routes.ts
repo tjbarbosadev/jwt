ea77f0511e2d62852e493013d320c4267b0a0d11
Thiago Barbosa <tjbarbosa.dev@gmail.com>
2026-03-23 17:10:25 -0300

    feat: verify user role for authorization

--- src/routes/products.routes.ts
+++ src/routes/products.routes.ts
@@ -1,11 +1,17 @@
 import { Router } from 'express';
 import { ProductsController } from '@/controllers/products-controller';
 import { ensureAuthenticated } from '@/middlewares/ensureAuthenticated';
+import { verifyUserAuthorization } from '@/middlewares/verifyUserAuthorization';
 
 const productsRoutes = Router();
 const productsController = new ProductsController();
 
-productsRoutes.get('/', ensureAuthenticated, productsController.index);
-productsRoutes.post('/', ensureAuthenticated, productsController.create);
+productsRoutes.get('/', productsController.index);
+productsRoutes.post(
+  '/',
+  ensureAuthenticated,
+  verifyUserAuthorization(['sale', 'admin']),
+  productsController.create,
+);
 
 export { productsRoutes };
